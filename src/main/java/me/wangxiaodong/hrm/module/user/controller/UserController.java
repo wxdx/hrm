d12b5eccf6528748b257769805520c578f8c0667
wangxiaodong <lle@live.com>
2020-02-29 12:48:23 +0800

    update

--- src/main/java/me/wangxiaodong/hrm/module/user/controller/UserController.java
+++ src/main/java/me/wangxiaodong/hrm/module/user/controller/UserController.java
@@ -1,22 +1,26 @@
 package me.wangxiaodong.hrm.module.user.controller;
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import me.wangxiaodong.hrm.base.RespEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api")
 @Api(description = "用户相关接口")
 public class UserController {
 
-    @ApiOperation(value = "接口的功能介绍",notes = "提示接口使用者注意事项",httpMethod = "GET")
-    @ApiImplicitParam(dataType = "string",name = "name",value = "姓名",required = true)
-    @RequestMapping(value = "/")
-    public RespEntity index(String name) {
+    @ApiOperation(value = "新建用户",notes = "新建用户",httpMethod = "POST",response = RespEntity.class)
+    @PostMapping(value = "/user")
+    public RespEntity addUser(String name) {
 
         return RespEntity.success(name);
+    }
+
+    @ApiOperation(value = "删除用户",notes = "删除用户",httpMethod = "DELETE",response = RespEntity.class)
+    @DeleteMapping(value = "/user")
+    public RespEntity deleteUser(@RequestParam("userId") String userId) {
+
+        return RespEntity.success(userId);
     }
 }

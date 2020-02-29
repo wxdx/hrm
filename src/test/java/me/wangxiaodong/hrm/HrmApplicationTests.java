60f32263f1efe66a2d08a39111da8970555fa4d4
wangxiaodong <lle@live.com>
2020-02-29 10:39:55 +0800

    init

---
+++ src/test/java/me/wangxiaodong/hrm/HrmApplicationTests.java
@@ -0,0 +1,13 @@
+package me.wangxiaodong.hrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
